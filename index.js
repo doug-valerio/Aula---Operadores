57751737cecf622fb9d16e9f459ee3621cf26441
doug valerio <doug.valerio1985@gmail.com>
2023-04-12 21:47:06 -0300

    Exercicio de operadores

---
+++ index.js
@@ -0,0 +1,12 @@
+let kelvin = (77-32)*(5/9) + 273.15  
+console.log(kelvin)
+//
+let fahrenheit = (80)*(9/5) + 22
+console.log(fahrenheit)
+//
+let Celcius = (30)*(9/5) + 32
+console.log(Celcius)
+// 
+
+ let Celcius = 
+
